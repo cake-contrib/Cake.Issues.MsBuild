e6ca42b1e5769b57b36f12b5ae1b27a0c4482e57
Pascal Berger <pascal.berger@gmail.com>
2018-08-04 09:32:19 +0200

    Split aliases into partial classes

---
+++ src/Cake.Issues.MsBuild/MsBuildIssuesAliases.XmlFileLoggerFormat.cs
@@ -0,0 +1,49 @@
+﻿namespace Cake.Issues.MsBuild
+{
+    using Cake.Core;
+    using Cake.Core.Annotations;
+
+    /// <content>
+    /// Contains functionality related to <see cref="XmlFileLoggerFormat"/>.
+    /// </content>
+    public static partial class MsBuildIssuesAliases
+    {
+        /// <summary>
+        /// <para>
+        /// Gets an instance for the MsBuild log format as written by the <c>XmlFileLogger</c> class
+        /// from MSBuild Extension Pack.
+        /// </para>
+        /// <para>
+        /// You can add the logger to the MSBuildSettings like this:
+        /// <code>
+        /// var settings = new MSBuildSettings()
+        ///     .WithLogger(
+        ///         Context.Tools.Resolve("MSBuild.ExtensionPack.Loggers.dll").FullPath,
+        ///         "XmlFileLogger",
+        ///         string.Format(
+        ///             "logfile=\"{0}\";verbosity=Detailed;encoding=UTF-8",
+        ///             @"c:\build\msbuild.log")
+        ///     );
+        /// </code>
+        /// </para>
+        /// <para>
+        /// In order to use the above logger, include the following in your build.cake file to download and
+        /// install from NuGet.org:
+        /// <code>
+        /// #tool "nuget:?package=MSBuild.Extension.Pack"
+        /// </code>
+        /// </para>
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>Instance for the MsBuild log format.</returns>
+        [CakePropertyAlias]
+        [CakeAliasCategory(IssuesAliasConstants.IssueProviderCakeAliasCategory)]
+        public static MsBuildLogFileFormat MsBuildXmlFileLoggerFormat(
+            this ICakeContext context)
+        {
+            context.NotNull(nameof(context));
+
+            return new XmlFileLoggerFormat(context.Log);
+        }
+    }
+}
