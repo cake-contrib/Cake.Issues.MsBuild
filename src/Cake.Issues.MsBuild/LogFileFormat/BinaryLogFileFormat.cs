e9cd2258427bd7a7e8e731f45f8655a9b160669f
Pascal Berger <pascal.berger@gmail.com>
2019-10-05 10:49:11 +0200

    (GH-132) Update to Cake.Issues 0.8.0

--- src/Cake.Issues.MsBuild/LogFileFormat/BinaryLogFileFormat.cs
+++ src/Cake.Issues.MsBuild/LogFileFormat/BinaryLogFileFormat.cs
@@ -23,7 +23,6 @@
         /// <inheritdoc/>
         public override IEnumerable<IIssue> ReadIssues(
             MsBuildIssuesProvider issueProvider,
-            IssueCommentFormat format,
             RepositorySettings repositorySettings,
             MsBuildIssuesSettings issueProviderSettings)
         {
