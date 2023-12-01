ba3c20a97ff6b9b19f6374daf86bb01e9b4b7fa3
Pascal Berger <pascal.berger@swissgrc.com>
2023-12-01 22:23:01 +0100

    Add specific addin for Cake Frosting

--- src/Cake.Issues.MsBuild/MsBuildIssuesAliases.cs
+++ src/Cake.Issues.MsBuild/MsBuildIssuesAliases.cs
@@ -4,6 +4,9 @@
 
     /// <summary>
     /// Contains functionality for reading warnings from MSBuild log files.
+    ///
+    /// NOTE: Use Cake.Issues.Reporting.Generic addin to use these aliases with Cake Script Runners and
+    /// Cake.Frosting.Issues.Reporting.Generic to use these aliases with Cake Frosting.
     /// </summary>
     [CakeAliasCategory(IssuesAliasConstants.MainCakeAliasCategory)]
     public static partial class MsBuildIssuesAliases
