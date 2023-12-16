158da301b90ee2dbd3f34dddd3ef132ccce345db
Pascal Berger <pascal.berger@swissgrc.com>
2023-12-16 15:50:36 +0100

    Fix comment on alias class

--- src/Cake.Issues.MsBuild/MsBuildIssuesAliases.cs
+++ src/Cake.Issues.MsBuild/MsBuildIssuesAliases.cs
@@ -5,8 +5,8 @@
     /// <summary>
     /// Contains functionality for reading warnings from MSBuild log files.
     ///
-    /// NOTE: Use Cake.Issues.Reporting.Generic addin to use these aliases with Cake Script Runners and
-    /// Cake.Frosting.Issues.Reporting.Generic to use these aliases with Cake Frosting.
+    /// NOTE: Use Cake.Issues.MsBuild addin to use these aliases with Cake Script Runners and
+    /// Cake.Frosting.Issues.MsBuild to use these aliases with Cake Frosting.
     /// </summary>
     [CakeAliasCategory(IssuesAliasConstants.MainCakeAliasCategory)]
     public static partial class MsBuildIssuesAliases
